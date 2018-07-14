29af7f7ef65f63382dbfbce4ef2a0f9cad4b9db7
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-14 16:15:20 -0300

    refatorar class Aluno

--- app/src/main/java/br/com/alura/modelo/Aluno.kt
+++ app/src/main/java/br/com/alura/modelo/Aluno.kt
@@ -3,12 +3,12 @@ package br.com.alura.modelo
 import java.io.Serializable
 
 data class Aluno (
-    var id: Long?,
-    var nome: String?,
-    var endereco: String?,
-    var telefone: String?,
-    var site: String?,
-    var nota: Double ): Serializable {
+    val id: Long? = null,
+    val nome: String,
+    val endereco: String,
+    val telefone: String,
+    val site: String,
+    val nota: Double ): Serializable {
 
         override fun toString(): String {
             return id.toString() + " - " + nome
