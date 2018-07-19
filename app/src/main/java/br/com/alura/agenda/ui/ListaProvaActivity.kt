f4ac762ef0d49f451bc2d5efcbe0b9e93133d249
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-18 21:50:49 -0300

    activity da lista de provas

---
+++ app/src/main/java/br/com/alura/agenda/ui/ListaProvaActivity.kt
@@ -0,0 +1,38 @@
+package br.com.alura.agenda.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import br.com.alura.agenda.R
+import br.com.alura.agenda.modelo.Prova
+import kotlinx.android.synthetic.main.activity_lista_prova.*
+
+class ListaProvaActivity : AppCompatActivity() {
+
+    val provas: MutableList<Prova> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_prova)
+
+        val topicos1 = listOf("Sujeito", "Objeto direto", "Objeto indireto")
+        val prova1 = Prova("Portugues", "25/05/2016", topicos1)
+
+        val topicos2 = listOf("Equacoes de segundo grau", "Trigonometria")
+        val prova2 = Prova("Matematica", "27/05/2016", topicos2)
+
+        provas.addAll(listOf(prova1, prova2))
+
+        val adapter = ArrayAdapter<Prova>(this, android.R.layout.simple_list_item_1, provas)
+
+        listaProva.adapter = adapter
+        
+        listaProva.setOnItemClickListener { parent, _, posicao, _ ->
+            val prova: Prova = parent.getItemAtPosition(posicao) as Prova
+            val intent = Intent(this, ProvaActivity::class.java)
+            intent.putExtra("prova", prova)
+            startActivity(intent)
+        }
+    }
+}
