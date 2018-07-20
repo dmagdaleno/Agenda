77faa2f4948bafe10a53c00ac508443396f255fd
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-19 21:15:15 -0300

    substitui a activity de detalhes de prova pelo fragment

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaProvaActivity.kt
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaProvaActivity.kt
@@ -3,7 +3,12 @@ package br.com.alura.agenda.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.alura.agenda.R
+import br.com.alura.agenda.funcoes.isLandscapeMode
 import br.com.alura.agenda.ui.fragment.ListaProvaFragment
+import br.com.alura.agenda.ui.fragment.ProvaFragment
+import br.com.alura.agenda.modelo.Prova
+
+
 
 class ListaProvaActivity : AppCompatActivity() {
 
@@ -13,6 +18,29 @@ class ListaProvaActivity : AppCompatActivity() {
 
         val tx = supportFragmentManager.beginTransaction()
         tx.replace(R.id.frame_principal, ListaProvaFragment())
+        if(isLandscapeMode())
+            tx.replace(R.id.frame_secundario, ProvaFragment())
+
         tx.commit()
     }
+
+    fun selecionaProva(prova: Prova) {
+        val manager = supportFragmentManager
+        if(isLandscapeMode()){
+            val provaFragment = manager.findFragmentById(R.id.frame_secundario) as ProvaFragment
+            provaFragment.populaCamposCom(prova)
+        }
+        else {
+            val tx = manager.beginTransaction()
+
+            val provaFragment = ProvaFragment()
+            val parametros = Bundle()
+            parametros.putSerializable("prova", prova)
+            provaFragment.setArguments(parametros)
+
+            tx.replace(R.id.frame_principal, provaFragment)
+            tx.commit()
+        }
+    }
+
 }
