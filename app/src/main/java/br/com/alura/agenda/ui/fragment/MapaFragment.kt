567cbb1c0e5bf9780bd62c7c6490c0f888fb72d1
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-25 15:12:06 -0300

    adiciona novo fragmento de mapa com o posicionamento dos alunos

---
+++ app/src/main/java/br/com/alura/agenda/ui/fragment/MapaFragment.kt
@@ -0,0 +1,49 @@
+package br.com.alura.agenda.ui.fragment
+
+import android.location.Geocoder
+import android.os.Bundle
+import br.com.alura.agenda.dao.AlunoDAO
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapaFragment: SupportMapFragment(), OnMapReadyCallback{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val endereco = "R. Ibitirama, 1409 - Vila Prudente, São Paulo - SP"
+        val posicao = recuperaPosicaoPelo(endereco)
+        posicao?.let {
+            val update = CameraUpdateFactory.newLatLngZoom(posicao, 17f)
+            googleMap?.moveCamera(update)
+        }
+
+        val dao = AlunoDAO(context!!)
+        val alunos = dao.buscaAlunos()
+        alunos.forEach{ aluno ->
+            val posicao = recuperaPosicaoPelo(aluno.endereco)
+            posicao?.let {
+                val marcador = MarkerOptions()
+                marcador.position(it)
+                marcador.title(aluno.nome)
+                marcador.snippet(aluno.nota.toString())
+                googleMap?.addMarker(marcador)
+            }
+        }
+
+    }
+
+    private fun recuperaPosicaoPelo(endereco: String): LatLng? {
+        val geocoder = Geocoder(context)
+        val resultados = geocoder.getFromLocationName(endereco, 1)
+        if(!resultados.isEmpty())
+            return LatLng(resultados[0].latitude, resultados[0].longitude)
+
+        return null
+    }
+
+}
