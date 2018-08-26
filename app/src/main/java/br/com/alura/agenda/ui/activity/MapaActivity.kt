430bbb7a0e0963468a0b4b3cef92f890266229b5
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-26 17:03:45 -0300

    pedindo permissao para usar o gps

--- app/src/main/java/br/com/alura/agenda/ui/activity/MapaActivity.kt
+++ app/src/main/java/br/com/alura/agenda/ui/activity/MapaActivity.kt
@@ -1,18 +1,55 @@
 package br.com.alura.agenda.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import br.com.alura.agenda.R
+import br.com.alura.agenda.modelo.Localizador
 import br.com.alura.agenda.ui.fragment.MapaFragment
 
 class MapaActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSOES: Int = 1
+
+    private val mapaFragment = MapaFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
 
         val tx = supportFragmentManager.beginTransaction()
-        tx.replace(R.id.frame_mapa, MapaFragment())
+        tx.replace(R.id.frame_mapa, mapaFragment)
         tx.commit()
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(possuiPermissaoDeMovimentacao() || possuiPermissaoDeLocalizacao()) {
+                val permissoes = arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                requestPermissions(permissoes, REQUEST_PERMISSOES)
+            }
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == REQUEST_PERMISSOES){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Localizador(this, mapaFragment)
+            }
+        }
+    }
+
+    private fun possuiPermissaoDeMovimentacao() =
+            ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED
+
+    private fun possuiPermissaoDeLocalizacao() =
+            ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED
 }
