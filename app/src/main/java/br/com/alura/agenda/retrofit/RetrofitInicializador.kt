637a0fd4dff30c52e35de16314b9a086a424b8ee
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-27 22:53:49 -0300

    adiciona interceptor do okhttp para logar informações do retrofit

--- app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.kt
@@ -1,16 +1,28 @@
 package br.com.alura.agenda.retrofit
 
 import br.com.alura.agenda.retrofit.service.AlunoService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 class RetrofitInicializador {
 
-    val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.15.17:8080/api/")
-            .addConverterFactory(JacksonConverterFactory.create())
-            .build()
+    private val retrofit: Retrofit
 
     val alunoService: AlunoService
         get() = retrofit.create(AlunoService::class.java)
+
+    init {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        retrofit = Retrofit.Builder()
+                .baseUrl("http://192.168.15.17:8080/api/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(httpClient)
+                .build()
+    }
 }
