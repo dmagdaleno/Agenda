171c9d7681de6be478a78867084932139483c16f
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-29 19:31:46 -0300

    remove jsonproperty e muda tipo do id para string

--- app/src/main/java/br/com/alura/agenda/modelo/Aluno.kt
+++ app/src/main/java/br/com/alura/agenda/modelo/Aluno.kt
@@ -1,10 +1,9 @@
 package br.com.alura.agenda.modelo
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.io.Serializable
 
 data class Aluno (
-    @field:JsonProperty("idCliente") val id: Long? = null,
+    val id: String? = null,
     val nome: String,
     val endereco: String,
     val telefone: String,
