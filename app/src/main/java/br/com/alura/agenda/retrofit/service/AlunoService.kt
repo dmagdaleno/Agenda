e1b1364486ce554cdf6aad05993ce5c38c841bb0
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-29 21:58:34 -0300

    recupera lista de alunos do servidor

--- app/src/main/java/br/com/alura/agenda/retrofit/service/AlunoService.kt
+++ app/src/main/java/br/com/alura/agenda/retrofit/service/AlunoService.kt
@@ -1,12 +1,17 @@
 package br.com.alura.agenda.retrofit.service
 
 import br.com.alura.agenda.modelo.Aluno
+import br.com.alura.agenda.retrofit.service.dto.ListaAlunoDTO
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AlunoService {
 
     @POST("aluno")
     fun insere(@Body aluno: Aluno): Call<Unit>
+
+    @GET("aluno")
+    fun lista(): Call<ListaAlunoDTO>
 }
